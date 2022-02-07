bbb366bbeb151247d90b6f9130148da79e150bf7
mateusbra <suehtamzarb@gmail.com>
2022-02-07 19:52:56 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React oi
+          Learn React ola
         </a>
       </header>
     </div>
