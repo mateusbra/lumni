6b1f86a4515a0bd8b8e84608abd5987814e9842a
mateusbra <suehtamzarb@gmail.com>
2022-03-17 12:33:33 -0300

    contato

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,13 @@ import Sobre from "../assets/sobre.png";
 import styles from "../styles/styles";
 
 function About(){
+
+        let navigate = useNavigate();
+        const changeToContact = () =>{ 
+            let path = `contato`; 
+            navigate(path);
+        }
+
     return(
       <div style={{width:"100%",height:768,backgroundColor:"#000f40"}}>
         <div style={{...styles.aboutContainer,backgroundImage:`url(${Sobre})`}}>
@@ -11,7 +18,7 @@ function About(){
                 <div style={{marginTop:100,maxWidth:700}}>
                   <div style={{color:"#07091d",fontWeight:"bolder",fontSize:70,fontFamily:"FFGoodPro-Bold"}}>Sobre a Lumni</div>
                   <div style={{marginTop:45,color:"white",fontSize:30,fontFamily:"FFGoodPro-Regular"}}>A Lumni é um studio gaúcho de design gráfico que busca guiar empresas para o sucesso. Contamos com profissionais de diferentes áreas para atender a sua necessidade e oferecer um serviço único de qualidade.</div>
-                  <button style={{...styles.homeButton,display:"flex",margin:"100px auto 0"}}>ENTRE EM CONTATO</button>
+                  <button onClick={changeToContact} style={{...styles.homeButton,display:"flex",margin:"100px auto 0"}}>ENTRE EM CONTATO</button>
                   </div>
               </div>
           </div>
