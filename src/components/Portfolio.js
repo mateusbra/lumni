8ce73c9ddef7d48beef81b8884b643d23178e814
mateusbra <suehtamzarb@gmail.com>
2022-03-21 18:37:38 -0300

    last changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,11 +4,18 @@ import Capa from "../../src/assets/capa-portfolio.png";
 
 function Portfolio(){
     return(
-        <div id="Portfolio" style={{width:"100%",height:850,backgroundColor:"#000f40"}}>
-            <div style={{width:"100%",margin:"auto"}}>
-                <img alt={"Portfolio"} style={{width:"100%",height:750,margin:"auto"}} src={Capa}/>
-            </div>
-                <button className="click" onClick={() => {window.open("https://www.behance.net/studiolumni", '_blank').focus()}} style={{...styles.homeButton,fontWeight:"bolder",padding:"5px 28px",backgroundColor:styles.colors.orange,borderStyle:"none",margin:"30px auto 0px",display:"flex"}}>VER TODO O PORTFÓLIO</button>
+        <div id="Portfolio" style={{width:"100%",height:950,backgroundColor:"#000f40"}}>
+            <div id="Home" style={{...styles.lightHouseContainer,width:"100%",height:850,backgroundImage:`url(${Capa})`,backgroundSize:"cover"}}>
+          <div style={{marginLeft:100}}>
+              <div style={{display:"flex",flexDirection:"column"}}>
+                <div style={{marginLeft:200,marginTop:300,width:500}}>
+                  <div style={{color:styles.colors.orange,fontSize:50,fontFamily:"FFGoodPro-Bold"}}>CONHEÇA NOSSO</div>
+                  <div style={{color:styles.colors.orange,fontSize:130,fontFamily:"FFGoodPro-Bold"}}>PORTFÓLIO</div>
+                </div>
+              </div> 
+          </div>
+          </div>
+                <button onClick={() => {window.open("https://www.behance.net/studiolumni", '_blank').focus()}} style={{...styles.homeButton,fontWeight:"bolder",padding:"5px 28px",backgroundColor:styles.colors.orange,borderStyle:"none",margin:"32px auto 0px",display:"flex"}}>VER TODO O PORTFÓLIO</button>
         </div>
     );
 }
